6bc6aae47e35c9871797f89f348b1d5b06d3b82e
ridia13 <68280420+ridia13@users.noreply.github.com>
2021-05-01 01:25:34 +0900

    HTML+CSS+JS : saving the userName

--- clock.js
+++ clock.js
@@ -3,6 +3,7 @@
 const clock = document.querySelector('.js-clock'),
   title = clock.querySelector('.js-title');
 
+
 function getTime() {
   const date = new Date();
   const hour = date.getHours();
@@ -11,7 +12,10 @@ function getTime() {
   title.innerText = `${hour < 10? `0${hour}` : `${hour}`}:${min < 10? `0${min}` : `${min}`}:${sec < 10? `0${sec}` : `${sec}`}`;
 }
 
-function init(){
+
+
+
+function init() {
   setInterval(getTime, 1000);
 }
 init();
